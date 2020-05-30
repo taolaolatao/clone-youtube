ac48c8a57eb8718c5c6368e1a8c25fc1e5286661
VHM <taolaolataom@gmail.com>
2020-05-30 17:30:27 +0700

    Updates

---
+++ precache-manifest.03e115ceb5ad921835d2bcb6b37bce43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d8ea5850ad18c4ba765a3967df208b",
+    "url": "/clone-youtube/index.html"
+  },
+  {
+    "revision": "fe146800ce231d6c4457",
+    "url": "/clone-youtube/static/js/2.2e2e880b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/clone-youtube/static/js/2.2e2e880b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37af9e4c20f4b3bc89b5",
+    "url": "/clone-youtube/static/js/main.37a192a1.chunk.js"
+  },
+  {
+    "revision": "2cfc6cf58b43c5355f79",
+    "url": "/clone-youtube/static/js/runtime-main.d76b8cb2.js"
+  }
+]);
